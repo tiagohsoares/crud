57f32aa20e7f6d06fc9973986863b92802aedb3c
tiagohsoares <tiagowenceslau11@gmail.com>
2025-05-19 12:00:15 -0300

    add: class connect

--- connect.php
+++ connect.php
@@ -1,19 +1,16 @@
 <?php
  //Conectar Banco de dados MySQL
  class conexao {
-
     public $db;
+
+    public function db ($db){
+        $this->db = $db; 
+    }
     
-function test (){
-
-
- try {
-    $db = new PDO("mysql:host=localhost;port=3306;dbname=businessdb;charset=utf8mb4", "root", "",);
+    public function conecte($db){
+    $this->db = new PDO("mysql:host=localhost;port=3306;dbname=businessdb;charset=utf8mb4", "root", "",);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Conexao bem-sucedida!";
- } catch (Exception $e) {
-    echo "Sem conexao";
-    die();
- }
+    return $db;
+    }
 }
- }
